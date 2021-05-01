b06632318bc8250688d176c27d1b6a7dbacd6f71
Confuze <jansuchanek0@gmail.com>
2021-05-01 19:18:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const fs = require("fs").promises;
+let images;
+(async function () {
+	images = await fs.readdir("./src/img", "utf-8");
+})();
 
 app.use(express.static("src"));
 
@@ -13,9 +17,14 @@ app.get("/about", async (req, res) => {
 });
 
 app.get("/images", async (req, res) => {
-	res.status(200).send(await fs.readdir("./src/img", "utf-8"));
+	res.status(200).send(await images);
 });
 
+app.get("/piec", async (req, res) => {
+	res.status(200).send({ link: images[Math.floor(Math.random() * images.length)] });
+});
+
+// images[Math.floor(Math.random * images.length)]
 app.listen(process.env.PORT || 3000, () => {
 	console.log(`Listening on port ${process.env.PORT || 3000}`);
 });
